8503c435f2574c860c79441594f782643d614ddb
Pavel Sychev <pasha.sychev@gmail.com>
2016-05-28 15:03:40 +0300

    Fixed tuned_leveldb implementation.

--- util/tuned_leveldb.py
+++ util/tuned_leveldb.py
@@ -7,34 +7,34 @@ class LevelDB:
     def __init__(self, *args, **kwargs) -> None:
         self.db = OriginalLevelDB(*args, **kwargs)
 
-    def collection(self, collection_name: str) -> LevelDBCollection:
+    def collection(self, collection_name: str) -> 'LevelDBCollection':
         return LevelDBCollection(self, collection_name)
 
     def get(self, key: str, fill_cache=True) -> dict:
-        return loads(self.db.Get(key=key.encode('utf8'), fill_cache=fill_cache).decode('utf8'))
+        return loads(self.db.Get(key=key.encode(), fill_cache=fill_cache).decode())
 
     def put(self, key: str, value: dict, sync=True):
-        return self.db.Put(key=key.encode('utf8'), value=dumps(value, ensure_ascii=False).encode('utf8'), sync=sync)
+        return self.db.Put(key=key.encode(), value=dumps(value, ensure_ascii=False).encode(), sync=sync)
 
     def iterate_all(self, include_value=True, verify_checksums=False,
                     fill_cache=True) -> 'Iterator[Tuple[str, Optional[dict]]]':
         it = self.db.RangeIter(include_value=include_value, verify_checksums=verify_checksums, fill_cache=fill_cache)
         if include_value:
             for key, value in it:
-                yield key.decode('utf8'), loads(value.decode('utf8'))
+                yield key.decode(), loads(value.decode())
         else:
             for key in it:
-                yield key.decode('utf8'), None
+                yield key.decode(), None
 
 
 class LevelDBCollection:
     def __init__(self, db: LevelDB, collection: str):
-        assert '@' not in self.collection, 'Collection name should not have Symbol \'@\' in it!'
+        assert '@' not in collection, 'Collection name should not have Symbol \'@\' in it!'
         self.db = db
         self.collection = collection
-        self.begin_key = '{}@a'.format(self.collection)
-        self.data_key_prefix = '{}@b'.format(self.collection)
-        self.end_key = '{}@c'.format(self.collection)
+        self.begin_key = '{}@<'.format(self.collection)
+        self.data_key_prefix = '{}@='.format(self.collection)
+        self.end_key = '{}@>'.format(self.collection)
 
         self.db.put(self.begin_key, dict())
         self.db.put(self.end_key, dict())
@@ -53,16 +53,16 @@ class LevelDBCollection:
 
     def iterate_all(self, include_value=True, verify_checksums=False,
                     fill_cache=True) -> 'Iterator[Tuple[str, Optional[dict]]]':
-        it = self.db.db.RangeIter(key_from=self.begin_key, key_to=self.end_key,
+        it = self.db.db.RangeIter(key_from=self.begin_key.encode(), key_to=self.end_key.encode(),
                                   include_value=include_value,
                                   verify_checksums=verify_checksums, fill_cache=fill_cache)
         if include_value:
             for key, value in it:
-                key = key.decode('utf8')
+                key = key.decode()
                 if key not in (self.begin_key, self.end_key):
-                    yield self._from_full_key(key), loads(value.decode('utf8'))
+                    yield self._from_full_key(key), loads(value.decode())
         else:
             for key in it:
-                key = key.decode('utf8')
+                key = key.decode()
                 if key not in (self.begin_key, self.end_key):
                     yield self._from_full_key(key), None
