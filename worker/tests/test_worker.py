f787740b6b4c6d797035d3f14ff2afae2aa6fd47
Pavel Sychev <pasha.sychev@gmail.com>
2016-05-29 21:21:02 +0300

    More tests for worker.

--- worker/tests/test_worker.py
+++ worker/tests/test_worker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import requests
+from time import sleep
 
 
 def main():
@@ -9,6 +10,12 @@ def main():
     print(task_id)
     start_result = requests.post('http://localhost:8080/v1.0/task/{}/start'.format(task_id))
     print(start_result.json())
+    while True:
+        sleep(1)
+        state = requests.get('http://localhost:8080/v1.0/task/{}/state'.format(task_id)).json()
+        print(state)
+        if state['payload']['state'] != 'running':
+            break
 
 
 if __name__ == '__main__':
