f2eaaa384a6d7da4b6e685b6a64082094c031ec7
Pavel Sychev <pasha.sychev@gmail.com>
2016-05-29 18:25:16 +0300

    Fixed route.

--- worker/app.py
+++ worker/app.py
@@ -92,7 +92,7 @@ class WorkerApi(CommonApi):
             ('GET', '/v1.0/tasks', 'list_tasks'),
             ('POST', '/v1.0/task/', 'create_task'),
             ('GET',  '/v1.0/task/{task_id}', 'get_task_info'),
-            ('GET',  '/v1.0/task/{task_id}/state', 'read_task_state'),
+            ('GET',  '/v1.0/task/{task_id}/state', 'get_task_state'),
             ('POST', '/v1.0/task/{task_id}/start', 'start_task'),
             ('POST', '/v1.0/task/{task_id}/stop', 'stop_task'),
         ]
